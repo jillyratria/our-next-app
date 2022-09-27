9e96edceca70da803624817c81c9b4d1a7ca70eb
Jilly Ratria <jilly@ruangguru.com>
2022-09-27 16:14:44 +0700

    init jilly page and create MainPage component

---
+++ pages/jilly.jsx
@@ -0,0 +1,10 @@
+import { MainPage } from "../components/MainPage";
+import styles from "../styles/Home.module.css";
+
+export default function JillyPage() {
+  return (
+    <MainPage pageTitle="Jilly's Page">
+      <h2>test test</h2>
+    </MainPage>
+  );
+}
