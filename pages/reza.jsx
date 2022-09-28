639ae6ed35ac55b4df34d210b17316767ec4f46e
reza.maulana20 <reza.maulana@ruangguru.com>
2022-09-28 17:29:59 +0700

    first push

---
+++ pages/reza.jsx
@@ -0,0 +1,10 @@
+import { MainPage } from "../components/MainPage";
+import styles from "../styles/Home.module.css";
+
+export default function TodoPage() {
+  return (
+    <MainPage pageTitle="To Do App">
+      <h2>TO DO APP</h2>
+    </MainPage>
+  );
+}
