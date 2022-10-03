ab8ed2abbe7867cf90c4a0b35d999ce1556ddb63
Hofar Ismail <g9.hofar.ismail@gmail.com>
2022-10-03 17:26:16 +0700

    belajar

---
+++ pages/hofar.jsx
@@ -0,0 +1,10 @@
+import { MainPage } from "../components/MainPage";
+import styles from "../styles/Home.module.css";
+
+export default function HofarPage() {
+    return (
+        <MainPage pageTitle="Hofar's Page">
+            <h2>TO DO APP</h2>
+        </MainPage>
+    );
+}
