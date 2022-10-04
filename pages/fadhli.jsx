145ee1413b42c876ce83e9b5e3ce31d161fdc259
Ghazian Fadhli Fakhrusy <fadhlighazian@gmail.com>
2022-10-04 14:46:34 +0700

    initial commit

---
+++ pages/fadhli.jsx
@@ -0,0 +1,10 @@
+import { MainPage } from '../components/MainPage';
+import styles from '../styles/Home.module.css';
+
+export default function FadhliPage() {
+  return (
+    <MainPage pageTitle="Fadhli's Page">
+      <h2>TO DO APP</h2>
+    </MainPage>
+  );
+}
