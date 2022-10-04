94fe884bae83d8cd1982f3cb52a40aa886d54b1e
jillyratria <mail@jilly.web.id>
2022-10-04 17:14:04 +0700

    feat: add new component <BoxLink /> for main page (#11)
    Co-authored-by: Jilly Ratria <jilly@ruangguru.com>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { BoxLink } from "../components/BoxLink";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const membersTodoList = [
+    {
+      href: "novi",
+      title: "Novi",
+      description: "To do App by Novi",
+    },
+    {
+      href: "jilly",
+      title: "Jilly",
+      description: "To do App by Jilly",
+    },
+  ];
   return (
     <div className={styles.container}>
       <Head>
@@ -16,10 +29,20 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>Pages for each member</p>
+        <div className={styles.grid}>
+          {membersTodoList.map((m, idx) => {
+            return (
+              <BoxLink
+                key={idx}
+                href={m.href}
+                title={m.title}
+                description={m.description}
+              />
+            );
+          })}
+        </div>
+        <p className={styles.description}>Dokumentasi</p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -58,12 +81,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
